2ce32fce421c156c690be856d11c65d93fee6729
Pedro Galan <pedro.galan@piksel.com>
2020-06-02 16:56:40 +0100

    Create config server

---
+++ time-config-server/src/test/java/es/pedrogalan/time/configserver/TimeConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.pedrogalan.time.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
