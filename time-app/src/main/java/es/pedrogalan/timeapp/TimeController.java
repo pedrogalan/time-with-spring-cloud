15838e4c8e9105930a84ac9a7503481c582f00d4
Pedro Galan <pedro.galan@piksel.com>
2020-06-02 12:42:18 +0100

    Return the current datetime (not yet formatted)

--- time-app/src/main/java/es/pedrogalan/timeapp/TimeController.java
+++ time-app/src/main/java/es/pedrogalan/timeapp/TimeController.java
@@ -3,11 +3,13 @@ package es.pedrogalan.timeapp;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class TimeController {
 
     @RequestMapping("/")
     public String time() {
-        return "this will be the current time";
+        return LocalDateTime.now().toString();
     }
 }
