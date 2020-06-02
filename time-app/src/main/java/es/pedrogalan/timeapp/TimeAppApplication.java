f8e8f1fd9ca7f31c3628ed9fdc58bc415f54a833
Pedro Galan <pedro.galan@piksel.com>
2020-06-02 13:18:27 +0100

    Register time-app in Eureka

--- time-app/src/main/java/es/pedrogalan/timeapp/TimeAppApplication.java
+++ time-app/src/main/java/es/pedrogalan/timeapp/TimeAppApplication.java
@@ -2,7 +2,9 @@ package es.pedrogalan.timeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TimeAppApplication {
 
